3643cc0b025a181b27ba9ca973cf82416ccfc47f
maksiplus19 <maksiplus19@gmail.com>
2019-09-10 22:49:21 +0300

    Добавлена загрузка из матрицы смежности

--- graph/graph.py
+++ graph/graph.py
@@ -8,6 +8,8 @@ class Graph:
         self.adjacency_matrix = []
 
     def load_from_adjacency_list(self, file_name: str):
+        # сбрасываем старую матрицу
+        self.adjacency_matrix = []
         # максимальный номер вершины, чтобы позже выровнять матрицу до этого размера
         max_vertex_num = 0
         with open(file_name, mode='r') as file:
@@ -23,9 +25,8 @@ class Graph:
                     continue
                 if len(vertices) == 1:
                     self.adjacency_matrix.append([])
-                # сохраняем первую вершину и создаем для нее список
-                for i in range(len(vertices)):
-                    vertices[i] = int(vertices[i])
+                # сохраняем первую вершину и преобразуем вершины с весами в числа
+                vertices = [int(v) for v in vertices]
                 v = vertices[0]
                 # self.adjacency_matrix.append([])
                 if v > max_vertex_num:
@@ -33,14 +34,14 @@ class Graph:
                         self.adjacency_matrix.append([])
                     max_vertex_num = v
                 if len(self.adjacency_matrix[v]) < v:
-                    self.adjacency_matrix[v].extend([math.inf]*(v - len(self.adjacency_matrix[v]) + 1))
+                    self.adjacency_matrix[v].extend([math.inf] * (v - len(self.adjacency_matrix[v]) + 1))
                 # добавляем смежные вершины
                 for i in range(1, len(vertices), 2):
                     # сохраняем максимальный номер вершины
                     if vertices[i] > max_vertex_num:
                         max_vertex_num = vertices[i]
 
-                    # добовляем элементы если размер меньше, чем максимальный номер вершины
+                    # добовляем пустые элементы если размер меньше, чем максимальный номер вершины
                     while len(self.adjacency_matrix) - 1 < max_vertex_num:
                         self.adjacency_matrix.append([])
                     while len(self.adjacency_matrix[v]) - 1 < max_vertex_num:
@@ -48,6 +49,7 @@ class Graph:
 
                     # сохраняем веса в матрицу
                     self.adjacency_matrix[v][vertices[i]] = vertices[i + 1]
+
         while len(self.adjacency_matrix) - 1 < max_vertex_num:
             self.adjacency_matrix.append([])
         for i in range(len(self.adjacency_matrix)):
@@ -56,6 +58,17 @@ class Graph:
 
         for line in self.adjacency_matrix:
             print(line)
+
+    def load_from_adjacency_matrix(self, file_name: str):
+        # сбрасываем старую матрицу
+        self.adjacency_matrix = []
+
+        with open(file_name, mode='r') as file:
+            for line in file:
+                # разделяем строку на веса по пробелу
+                weights = line.split(' ')
+                # преобразуем строки в числа и сразу добавляем в матрицу
+                self.adjacency_matrix.append([int(w) for w in weights])
 
     def add_vertex(self):
         pass
