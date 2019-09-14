518c548859e0fb343b4f7902aa46b71b9416a3ce
maksiplus19 <Niklvitoz1>
2019-09-14 21:55:44 +0300

    Load and save methods delegated to special class. Added methods "add_edge", "add_vertex", "del_edge", "del_vertex", "clear"

--- graph/graph.py
+++ graph/graph.py
@@ -1,84 +1,23 @@
-# класс графа
-import math
-
-
 class Graph:
+    """Класс графа"""
     def __init__(self):
-        # граф хранится в виде матрицы смежности
-        self.adjacency_matrix = []
+        # граф хранится в виде словаря.
+        # Каждой вершине соответсвуеет словарь вершин,
+        # до которых есть дуги и список их весов
+        # vertexes = dict(name, dict(name, list))
+        self.vertexes = {}
 
-    def load_from_adjacency_list(self, file_name: str):
-        # сбрасываем старую матрицу
-        self.adjacency_matrix = []
-        # максимальный номер вершины, чтобы позже выровнять матрицу до этого размера
-        max_vertex_num = 0
-        with open(file_name, mode='r') as file:
-            # построчное чтение файла
-            for line in file:
-                # убираем комментарии
-                line = line.split('%', maxsplit=2)[0]
-                # разделяем строку на номера вершин
-                vertices = line.split(' ')
-                if len(vertices) % 2 == 0:
-                    raise Exception('Wrong file content')
-                if len(vertices) == 0:
-                    continue
-                if len(vertices) == 1:
-                    self.adjacency_matrix.append([])
-                # сохраняем первую вершину и преобразуем вершины с весами в числа
-                try:
-                    vertices = [int(v) for v in vertices]
-                except ValueError:
-                    self.adjacency_matrix = []
-                    return
-                v = vertices[0]
-                # self.adjacency_matrix.append([])
-                if v > max_vertex_num:
-                    for i in range(v - max_vertex_num + 1):
-                        self.adjacency_matrix.append([])
-                    max_vertex_num = v
-                if len(self.adjacency_matrix[v]) < v:
-                    self.adjacency_matrix[v].extend([math.inf] * (v - len(self.adjacency_matrix[v]) + 1))
-                # добавляем смежные вершины
-                for i in range(1, len(vertices), 2):
-                    # сохраняем максимальный номер вершины
-                    if vertices[i] > max_vertex_num:
-                        max_vertex_num = vertices[i]
-
-                    # добовляем пустые элементы если размер меньше, чем максимальный номер вершины
-                    while len(self.adjacency_matrix) - 1 < max_vertex_num:
-                        self.adjacency_matrix.append([])
-                    while len(self.adjacency_matrix[v]) - 1 < max_vertex_num:
-                        self.adjacency_matrix[v].append(math.inf)
-
-                    # сохраняем веса в матрицу
-                    self.adjacency_matrix[v][vertices[i]] = vertices[i + 1]
-
-        while len(self.adjacency_matrix) - 1 < max_vertex_num:
-            self.adjacency_matrix.append([])
-        for i in range(len(self.adjacency_matrix)):
-            if len(self.adjacency_matrix[i]) - 1 < max_vertex_num:
-                self.adjacency_matrix[i].extend([math.inf] * (max_vertex_num - len(self.adjacency_matrix[i]) + 1))
-
-        # for line in self.adjacency_matrix:
-        #     print(line)
-
-    def load_from_adjacency_matrix(self, file_name: str):
-        # сбрасываем старую матрицу
-        self.adjacency_matrix = []
-
-        with open(file_name, mode='r') as file:
-            for line in file:
-                # разделяем строку на веса по пробелу
-                weights = line.split(' ')
-                # преобразуем строки в числа и сразу добавляем в матрицу
-                self.adjacency_matrix.append([int(w) for w in weights])
-
-    def load_from_incidence_matrix(self, filr_name: str):
+    def add_edge(self, v_from: str, v_to: str, weight: int = 1):
         pass
 
-    def load_from_arc_list(self, file_name: str):
+    def add_vertex(self, name: str, x: int, y: float):
         pass
 
-    def add_vertex(self):
+    def del_edge(self, v_from: str, v_to: str, weight: int):
         pass
+
+    def del_vertex(self):
+        pass
+
+    def clear(self):
+        self.vertexes.clear()
