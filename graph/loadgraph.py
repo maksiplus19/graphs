87d1d164db4bc70062d038d9842fd675e7ae03cb
maksiplus19 <Niklvitoz1>
2019-09-29 19:10:09 +0300

    Refactor

--- graph/loadgraph.py
+++ graph/loadgraph.py
@@ -28,8 +28,6 @@ class LoadGraph:
 
         # десериализуем вершены-ребра
         graph.vertexes = json.loads(vertexes)
-        # устанавливаем счетчик вершин
-        graph.set_vertexes_counter(len(graph.vertexes))
         # загружем данные о координатах или генерируем, если их нет
         if v_coordinates is not None:
             v_coordinates = json.loads(v_coordinates)
@@ -39,64 +37,7 @@ class LoadGraph:
             for v in graph.vertexes:
                 graph.vertexes_coordinates[v] = Vertex(v, random.randint(0, 100), random.randint(0, 100))
 
-
     @staticmethod
-    def load_from_adjacency_list(graph: Graph, file_name: str):
-        # # сбрасываем старую матрицу
-        # graph.vertexes = {}
-        # # максимальный номер вершины, чтобы позже выровнять матрицу до этого размера
-        # max_vertex_num = 0
-        # with open(file_name, mode='r') as file:
-        #     # построчное чтение файла
-        #     for line in file:
-        #         # убираем комментарии
-        #         line = line.split('%', maxsplit=2)[0]
-        #         # разделяем строку на номера вершин
-        #         vertices = line.split(' ')
-        #         if len(vertices) % 2 == 0:
-        #             raise Exception('Wrong file content')
-        #         if len(vertices) == 0:
-        #             continue
-        #         if len(vertices) == 1:
-        #             self.adjacency_matrix.append([])
-        #         # сохраняем первую вершину и преобразуем вершины с весами в числа
-        #         try:
-        #             vertices = [int(v) for v in vertices]
-        #         except ValueError:
-        #             self.adjacency_matrix = []
-        #             return
-        #         v = vertices[0]
-        #         # self.adjacency_matrix.append([])
-        #         if v > max_vertex_num:
-        #             for i in range(v - max_vertex_num + 1):
-        #                 self.adjacency_matrix.append([])
-        #             max_vertex_num = v
-        #         if len(self.adjacency_matrix[v]) < v:
-        #             self.adjacency_matrix[v].extend([math.inf] * (v - len(self.adjacency_matrix[v]) + 1))
-        #         # добавляем смежные вершины
-        #         for i in range(1, len(vertices), 2):
-        #             # сохраняем максимальный номер вершины
-        #             if vertices[i] > max_vertex_num:
-        #                 max_vertex_num = vertices[i]
-        #
-        #             # добовляем пустые элементы если размер меньше, чем максимальный номер вершины
-        #             while len(self.adjacency_matrix) - 1 < max_vertex_num:
-        #                 self.adjacency_matrix.append([])
-        #             while len(self.adjacency_matrix[v]) - 1 < max_vertex_num:
-        #                 self.adjacency_matrix[v].append(math.inf)
-        #
-        #             # сохраняем веса в матрицу
-        #             self.adjacency_matrix[v][vertices[i]] = vertices[i + 1]
-        #
-        # while len(self.adjacency_matrix) - 1 < max_vertex_num:
-        #     self.adjacency_matrix.append([])
-        # for i in range(len(self.adjacency_matrix)):
-        #     if len(self.adjacency_matrix[i]) - 1 < max_vertex_num:
-        #         self.adjacency_matrix[i].extend([math.inf] * (max_vertex_num - len(self.adjacency_matrix[i]) + 1))
-        pass
-
-    @staticmethod
-
     def __isfloat(value):
         try:
             float(value)
@@ -122,7 +63,6 @@ class LoadGraph:
                         graph.add_vertex(str(j), random.randint(-50, 100), random.randint(0, 100))
                         graph.add_edge(str(i), str(j), int(matrix1[i][j]))
 
-
     @staticmethod
     def load_from_incidence_matrix(graph: Graph, file_name: str):
         matrix = []
@@ -140,8 +80,6 @@ class LoadGraph:
                         graph.add_vertex(str(i), random.randint(-50, 100), random.randint(-50, 100))
                         graph.add_vertex(str(j), random.randint(-50, 100), random.randint(0, 100))
                         graph.add_edge(str(i), str(j), int(matrix[i][j]))
-
-
 
     @staticmethod
     def load_from_ribs_list(graph: Graph, file_name: str):
@@ -171,7 +109,6 @@ class LoadGraph:
                         graph.add_edge(params[2], params[1], int(params[0]))
                         graph.oriented = bool(int(params[3]))
                         params = []
-        graph.set_vertexes_counter(len(graph.vertexes))
 
     @staticmethod
     def load_from_arc_list(graph: Graph, file_name: str):
