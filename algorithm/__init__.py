ebc635237049f32d721c17f3ca64f36cc927e147
maksiplus19 <maksiplus19@gmail.com>
2019-12-09 15:53:24 +0300

    lab8 done

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -1,3 +1,5 @@
 from .path_find import BFS
 from .path_find import A_star
 from .path_find import IDA_star
+from .isomorphous_graphs import isomorphic
+from .binary_operation import binary_operation
