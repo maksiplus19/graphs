bd4e6839176d7164861e71cd70aece1f0a4cc812
maksiplus19 <maksiplus19@gmail.com>
2019-12-12 22:44:37 +0300

    Make a cork for radius method

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -3,5 +3,5 @@ from .path_find import A_star
 from .path_find import IDA_star
 from .isomorphous_graphs import isomorphic
 from .additional import additional
-# from .path_find import radius
+from .radius_other import radius
 from .binary_operation import binary_operation
--- algorithm/radius_other.py
+++ algorithm/radius_other.py
@@ -2,6 +2,6 @@ from graph.graph import Graph
 from graph.vertex import Vertex
 from algorithm.path_find import BFS
 
-# def radius(graph:Graph, begin: str):
-#     for
 
+def radius(graph: Graph, begin: str):
+    pass
