fdb4071cbbca455de04042f46a41939b0c0cb18b
maksiplus19 <maksiplus19@gmail.com>
2019-12-19 23:02:35 +0300

    Added import for lab17

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -14,3 +14,4 @@ from .connectedness import find_comps
 from .connectedness import find_bridges
 from .extreme_graph import extreme
 from .coloring import coloring
+from .complex2_from_vector import aggregator as get_complex_from_vector
